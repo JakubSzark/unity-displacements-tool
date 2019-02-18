c036f937752222bc4989a0ae1420e3db6e01adb8
Jakub <jakubshark@gmail.com>
2019-02-18 14:01:25 -0800

    Put all my scripts into Szark namespace and added comments

--- Assets/Scripts/Displacement.cs
+++ Assets/Scripts/Displacement.cs
@@ -1,96 +1,116 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(MeshCollider))]
-[RequireComponent(typeof(MeshRenderer))]
-[RequireComponent(typeof(MeshFilter))]
-
-[ExecuteInEditMode]
-[DisallowMultipleComponent]
-public class Displacement : MonoBehaviour 
+namespace Szark
 {
-    public Vector3[] Vertices { get; set; }
+    [RequireComponent(typeof(MeshCollider))]
+    [RequireComponent(typeof(MeshRenderer))]
+    [RequireComponent(typeof(MeshFilter))]
 
-    public MeshFilter filter;
-    private MeshCollider coll;
-
-    [SerializeField, HideInInspector]
-    private Vector3Int data;
-
-    private void OnEnable()
+    [ExecuteInEditMode]
+    [DisallowMultipleComponent]
+    public class Displacement : MonoBehaviour
     {
-        filter = GetComponent<MeshFilter>();
-        coll = GetComponent<MeshCollider>();
-    }
+        public Vector3[] Vertices { get; set; }
 
-    private void Update()
-    {
-        if (Vertices == null)
+        public MeshFilter filter;
+        private MeshCollider coll;
+
+        [SerializeField, HideInInspector]
+        private Vector3Int data;
+
+        private void OnEnable()
         {
-            filter.sharedMesh = Instantiate(filter.sharedMesh);
-            Vertices = filter.sharedMesh.vertices;
+            filter = GetComponent<MeshFilter>();
+            coll = GetComponent<MeshCollider>();
+        }
+
+        private void Update()
+        {
+            // Updates vertices in case this displacement is duplicated
+            if (Vertices == null)
+            {
+                filter.sharedMesh = Instantiate(filter.sharedMesh);
+                Vertices = filter.sharedMesh.vertices;
+                UpdateColliderMesh();
+            }
+        }
+
+        /// <summary>
+        /// Generates a plane
+        /// </summary>
+        public void Generate(int xSize, int zSize, int subDivisions)
+        {
+            if (xSize == 0 || zSize == 0 || subDivisions == 0) return;
+
+            filter.sharedMesh = new Mesh() { name = $"Displacement" };
+            data = new Vector3Int(xSize, zSize, subDivisions);
+            var subSize = new Vector2Int(xSize, zSize) * subDivisions;
+            Vertices = new Vector3[(subSize.x + 1) * (subSize.y + 1)];
+
+            var tangent = new Vector4(1f, 0f, 0f, -1f);
+            var tangents = new Vector4[Vertices.Length];
+            var uv = new Vector2[Vertices.Length];
+
+            for (int i = 0, y = 0; y <= subSize.y; y++)
+            {
+                for (int x = 0; x <= subSize.x; x++, i++)
+                {
+                    tangents[i] = tangent;
+                    uv[i] = new Vector2((float)x / subSize.x, (float)y / subSize.y);
+                    Vertices[i] = new Vector3((x - subSize.x * 0.5f) / subDivisions,
+                        0, (y - subSize.y * 0.5f) / subDivisions);
+                }
+            }
+
+            filter.sharedMesh.vertices = Vertices;
+            filter.sharedMesh.tangents = tangents;
+            filter.sharedMesh.uv = uv;
+
+            var triangles = new int[subSize.x * subSize.y * 6];
+
+            for (int ti = 0, vi = 0, y = 0; y < subSize.y; y++, vi++)
+            {
+                for (int x = 0; x < subSize.x; x++, ti += 6, vi++)
+                {
+                    triangles[ti] = vi;
+                    triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                    triangles[ti + 4] = triangles[ti + 1] = vi + subSize.x + 1;
+                    triangles[ti + 5] = vi + subSize.x + 2;
+                }
+            }
+
+            filter.sharedMesh.triangles = triangles;
+
+            RecalculateLighting();
             UpdateColliderMesh();
         }
-    }
 
-    public void Generate(int xSize, int zSize, int subDivisions)
-    {
-        filter.sharedMesh = new Mesh() { name = $"Displacement" };
-        data = new Vector3Int(xSize, zSize, subDivisions);
-        var subSize = new Vector2Int(xSize, zSize) * subDivisions;
-        Vertices = new Vector3[(subSize.x + 1) * (subSize.y + 1)];
+        /// <summary>
+        /// Updates the vertices of the displacment
+        /// </summary>
+        public void UpdateMeshVertices() =>
+            filter.sharedMesh.vertices = Vertices;
 
-        var tangent = new Vector4(1f, 0f, 0f, -1f);
-        var tangents = new Vector4[Vertices.Length];
-        var uv = new Vector2[Vertices.Length];
+        /// <summary>
+        /// Updates the collider to newest mesh
+        /// </summary>
+        public void UpdateColliderMesh() =>
+            coll.sharedMesh = filter.sharedMesh;
 
-        for (int i = 0, y = 0; y <= subSize.y; y++)
+        /// <summary>
+        /// Calculates bounds, normals, and tangents for lighting
+        /// </summary>
+        public void RecalculateLighting()
         {
-            for (int x = 0; x <= subSize.x; x++, i++)
-            {
-                tangents[i] = tangent;
-                uv[i] = new Vector2((float)x / subSize.x, (float)y / subSize.y);
-                Vertices[i] = new Vector3((x - subSize.x * 0.5f) / subDivisions, 
-                    0, (y - subSize.y * 0.5f) / subDivisions);
-            }
+            filter.sharedMesh.RecalculateBounds();
+            filter.sharedMesh.RecalculateTangents();
+            filter.sharedMesh.RecalculateNormals();
         }
 
-        filter.sharedMesh.vertices = Vertices;
-        filter.sharedMesh.tangents = tangents;
-        filter.sharedMesh.uv = uv;
-
-        var triangles = new int[subSize.x * subSize.y * 6];
-
-        for (int ti = 0, vi = 0, y = 0; y < subSize.y; y++, vi++)
-        {
-            for (int x = 0; x < subSize.x; x++, ti += 6, vi++)
-            {
-                triangles[ti] = vi;
-                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
-                triangles[ti + 4] = triangles[ti + 1] = vi + subSize.x + 1;
-                triangles[ti + 5] = vi + subSize.x + 2;
-            }
-        }
-
-        filter.sharedMesh.triangles = triangles;
-
-        RecalculateLighting();
-        UpdateColliderMesh();
-    }
-
-    public void UpdateMeshVertices() =>
-        filter.sharedMesh.vertices = Vertices;
-
-    public void UpdateColliderMesh() =>
-        coll.sharedMesh = filter.sharedMesh;
-
-    public void RecalculateLighting()
-    {
-        filter.sharedMesh.RecalculateBounds();
-        filter.sharedMesh.RecalculateTangents();
-        filter.sharedMesh.RecalculateNormals();
-    }
-
-    public void ResetToOriginal() {
-        Generate(data.x, data.y, data.z);
+        /// <summary>
+        /// Resets the displacement into a flat plane
+        /// </summary>
+        public void ResetToOriginal() =>
+            Generate(data.x, data.y, data.z);
     }
 }
